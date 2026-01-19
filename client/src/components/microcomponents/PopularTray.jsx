e1ec3c8f7be216b2caec74999c779d903bc82a35
Aditya <ranjanaditya452@gmail.com>
2026-01-20 04:21:53 +0530

    added functionality to popular tray on homepage

--- client/src/components/microcomponents/PopularTray.jsx
+++ client/src/components/microcomponents/PopularTray.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import StockContext from '../../context/context-creation/StockContext';
 
 const PopularTray = () => {
 
- let btnStyle="text-xl hover:cursor-pointer py-2 px-6 rounded-full border-1 border-[#e7e7e9] hover:bg-[#faf9fb]"
+  const {searchStock} = useContext(StockContext);
+
+  const handleClick = (symbol)=>{
+      searchStock(symbol);
+  }
+
+
+  let btnStyle="text-xl hover:cursor-pointer py-2 px-6 rounded-full border-1 border-[#e7e7e9] hover:bg-[#faf9fb]";
+  
   return (
     <div className='grid grid-cols-2 gap-3 md:grid-cols-3'>
-        <button className={`${btnStyle}`}>Nvidia</button>
-        <button className={`${btnStyle}`}>Meta</button>
-        <button className={`${btnStyle} col-span-2 md:col-span-1`}>Microsoft</button>
-        <button className={`${btnStyle}`}>HP</button>
-        <button className={`${btnStyle}`}>Intel</button>
-        <button className={`${btnStyle} col-span-2 md:col-span-1`}>Adobe</button>
+        <button onClick={()=>handleClick("NVDA")} className={`${btnStyle}`}>Nvidia</button>
+        <button onClick={()=>handleClick("META")} className={`${btnStyle}`}>Meta</button>
+        <button onClick={()=>handleClick("MSFT")} className={`${btnStyle} col-span-2 md:col-span-1`}>Microsoft</button>
+        <button onClick={()=>handleClick("HPQ")} className={`${btnStyle}`}>HP</button>
+        <button onClick={()=>handleClick("INTC")} className={`${btnStyle}`}>Intel</button>
+        <button onClick={()=>handleClick("ADBE")} className={`${btnStyle} col-span-2 md:col-span-1`}>Adobe</button>
     </div>
   )
 }
