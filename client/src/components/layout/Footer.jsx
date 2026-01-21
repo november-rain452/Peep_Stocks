626bf7cc70827f3abfc1801ee990744a8d1194aa
Aditya <ranjanaditya452@gmail.com>
2026-01-22 00:16:19 +0530

    links on footer open in a new tab

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
     <div className='h-12 flex justify-between items-center md:h-14 max-h-14 shrink-0 border-t border-gray-200'>
         <div className='flex gap-3.5 pl-6'>
             <div>&copy;Adi</div>
-            <a href='https://github.com/november-rain452/Peep_Stocks'>Repo</a>
+            <a href='https://github.com/november-rain452/Peep_Stocks' target="_blank" rel="noreferrer">Repo</a>
         </div>
         <div className='flex gap-3.5 pr-6 '>
             <a href=''>Terms</a>
-            <a href='https://github.com/november-rain452'>Github</a>
+            <a href='https://github.com/november-rain452' target="_blank" rel="noreferrer">Github</a>
         </div>
     </div>
   )
